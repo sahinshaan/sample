b75a62254fa9bc6c8d395381ba367aa3fbd41885
SahinShan <sahinshan.shan@gmail.com>
2020-06-18 09:13:56 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,92 @@
+
+<?php
+   ob_start();
+   session_start();
+?>
+
+<?
+   // error_reporting(E_ALL);
+   // ini_set("display_errors", 1);
+?>
+
+<html lang = "en">
+   
+   <head>
+      <title>Tutorialspoint.com</title>
+      <link href = "css/bootstrap.min.css" rel = "stylesheet">
+      <link rel="stylesheet" href="style.css">
+ 
+      
+   </head>
+	
+   <body>
+      
+      <h2>Enter Username and Password</h2> 
+      <div class = "container form-signin">
+         
+         <?php
+            $msg = '';
+		  	$save ='';
+		  
+		  
+		  
+		   if(isset($_POST['login']) && !empty($_POST['username']))
+		   {
+			   $user = $_POST['username']. "\n";
+			   $fp = fopen('data.txt','a+');
+			   if(fwrite($fp, $user))  {
+				   $save = "file saved";
+			   
+			   }
+			   
+			 fclose($fp);
+		   }
+            
+            if (isset($_POST['login']) && !empty($_POST['username']) 
+               && !empty($_POST['password'])) {
+				
+               if ($_POST['username'] == 'admin' && 
+                  $_POST['password'] == '1234') {
+                  $_SESSION['valid'] = true;
+                  $_SESSION['timeout'] = time();
+                  $_SESSION['username'] = 'admin';
+                  
+                  echo 'You have entered valid use name and password';
+               }else {
+                  $msg = 'Wrong username or password';
+               }
+            }
+         ?>
+      </div> <!-- /container -->
+      
+      <div class = "container">
+      
+         <form class = "form-signin" role = "form" 
+            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
+            ?>" method = "post">
+          
+			 
+			 <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
+            <input type = "text" class = "form-control" 
+               name = "username" placeholder = "username = admin" 
+               required autofocus></br>
+		  
+		  
+		  <div>
+            <input type = "password" class = "form-control"
+               name = "password" placeholder = "password = 1234" required>
+            <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
+               name = "login">Login</button>
+		  </div>
+		  
+		  <span class="savemsg"> <?php echo $save; ?> 
+		  	</span>
+		 
+         </form>
+			
+         Click here to clean <a href = "logout.php" tite = "Logout">Session.
+         
+      </div> 
+      
+   </body>
+</html>
