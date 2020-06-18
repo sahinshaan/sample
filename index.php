58f58bb7ea9d40085422bd009687a6dad07b4d02
SahinShan <sahinshan.shan@gmail.com>
2020-06-18 09:18:38 -0700

    comment removed

--- index.php
+++ index.php
@@ -7,6 +7,28 @@
 <?
    // error_reporting(E_ALL);
    // ini_set("display_errors", 1);
+
+/* Attempt MySQL server connection. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+$link = mysqli_connect("localhost", "root", "", "demo");
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+ 
+// Attempt insert query execution
+$sql = "INSERT INTO persons (first_name, last_name, email) VALUES ('Peter', 'Parker', 'peterparker@mail.com')";
+if(mysqli_query($link, $sql)){
+    echo "Records inserted successfully.";
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($link);
+}
+mysqli_close($link);
+
+
+
+
 ?>
 
 <html lang = "en">
